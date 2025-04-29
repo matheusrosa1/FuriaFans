5c881869c37635217a61d127416856e6805de164
Matheus Rosa <matheusrosataxa@gmail.com>
2025-04-28 22:04:11 -0300

    feat: css page atualizado

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,80 +1,25 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { IoPersonSharp } from "react-icons/io5";
-import { SiTheconversation } from "react-icons/si";
-import { MdLogout } from "react-icons/md";
 import FanCard from "@/components/FanCard";
-import { Button } from "@/components/Button";
-import { useAuth } from "@/contexts/AuthContext"; 
+import Navbar from "@/components/Navbar";
 import { useFanContext } from "@/contexts/FanContextType";
 
 export default function HomePage() {
-  const router = useRouter();
-  const { isLogged, setLogged } = useAuth();
   const { fans } = useFanContext();
-
-  const handleLoginClick = () => {
-    router.push("/login");
-  };
-
-  const handleRegisterClick = () => {
-    router.push("/register");
-  };
-
-  const handleDropsClick = () => {
-    router.push("/drops");
-  };
-
-  const handleProfileClick = () => {
-    router.push("/fan/me");
-  };
-
-  const handleLogoutClick = () => {
-    setLogged(false);
-  };
+  const router = useRouter();
 
   return (
     <main className="p-6 min-h-screen bg-[url(/Torcida-FURIA-IEM-Rio-Major-2022.jpg)]">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-3xl font-bold flex gap-5 text-white">
-          <img src="/furiaLogo.png" alt="Logo" width={50} />Fãs da Fúria
-        </h1>
+      <Navbar />
 
-        <div className="flex gap-2">
-          {!isLogged && (
-            <>
-              <Button label="Cadastrar-se" onClick={handleRegisterClick} />
-              <Button
-                label="Login"
-                onClick={handleLoginClick}
-                icon={<IoPersonSharp size={20} />}
-              />
-            </>
-          )}
-          {isLogged && (
-            <>
-              <Button
-                label="Meu perfil"
-                onClick={handleProfileClick}
-                icon={<IoPersonSharp size={20} />}
-              />
-              <Button
-                label="Logout"
-                onClick={handleLogoutClick}
-                icon={<MdLogout />}
-              />
-            </>
-          )}
-          <Button
-            label="Drops"
-            onClick={handleDropsClick}
-            icon={<SiTheconversation size={20} />}
-          />
-        </div>
+      <div className="flex justify-center items-center mb-8 mt-4">
+        <h1 className="text-3xl font-bold flex gap-5 text-white">
+          <img src="/furiaLogo.png" alt="Logo" width={50} /> Fãs da Fúria
+        </h1>
       </div>
 
-      <div className="flex flex-wrap gap-4 w-full">
+      <div className="flex flex-wrap gap-4 w-full justify-center">
         {fans.map((fan) => (
           <FanCard key={fan.id} {...fan} />
         ))}
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,21 +12,21 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="sticky top-0 z-50 bg-white/30 backdrop-blur-md shadow-md p-4 flex justify-center gap-4">
-      <Button label="Home" onClick={() => router.push("/")} reverseColor />
+    <nav className="sticky top-0 z-50 p-4 flex justify-center gap-4">
+      <Button label="Home" onClick={() => router.push("/")} />
 
       {isLogged ? (
         <>
-          <Button label="Favoritos" onClick={() => router.push("/favorites")} reverseColor />
-          <Button label="Drops" onClick={() => router.push("/drops")} reverseColor />
-          <Button label="Meu Perfil" onClick={() => router.push("/fan/me")} reverseColor />
-          <Button label="Logout" onClick={handleLogout} reverseColor />
+          <Button label="Favoritos" onClick={() => router.push("/favorites")}  />
+          <Button label="Drops" onClick={() => router.push("/drops")}  />
+          <Button label="Meu Perfil" onClick={() => router.push("/fan/me")}  />
+          <Button label="Logout" onClick={handleLogout}  />
         </>
       ) : (
         <>
-          <Button label="Cadastrar-se" onClick={() => router.push("/register")} reverseColor />
-          <Button label="Login" onClick={() => router.push("/login")} reverseColor />
-          <Button label="Drops" onClick={() => router.push("/drops")} reverseColor />
+          <Button label="Cadastrar-se" onClick={() => router.push("/register")}  />
+          <Button label="Login" onClick={() => router.push("/login")}  />
+          <Button label="Drops" onClick={() => router.push("/drops")}  />
         </>
       )}
     </nav>
