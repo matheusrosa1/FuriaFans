db07d59062e3bfdffe0fdb4d99f266c9fd86c6e1
Matheus Rosa <matheusrosataxa@gmail.com>
2025-05-02 21:36:03 -0300

    tests: fancard e fandetails

--- frontend/__tests__/components/FanCard.test.tsx
+++ frontend/__tests__/components/FanCard.test.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
 import FanCard from '@/components/FanCard';
-import { fanMock } from '../../mocks/fanMock';
 import { renderWithProviders } from '../../test-utils/renderWithProviders';
+import { fanMock } from '../../mocks/fanMock';
 
 
 jest.mock('next/navigation', () => ({
@@ -15,11 +15,46 @@ jest.mock('next/navigation', () => ({
   }),
 }));
 
-describe('FanCard', () => {
-  it('deve renderizar o nickname do fã', () => {
-    renderWithProviders(<FanCard {...fanMock} />);
 
+describe('FanCard', () => {
+  beforeEach(() => {
+    renderWithProviders(<FanCard {...fanMock} />);
+  });
+
+  it('deve renderizar o nickname do fã', () => {
     const fanName = screen.getByText(fanMock.nickname);
     expect(fanName).toBeInTheDocument();
   });
+
+  it('deve renderizar o jogo favorito do fã', () => {
+    const favoriteGame = screen.getByText((content, element) => {
+      return content.includes(fanMock.favoriteGame);
+    });
+    expect(favoriteGame).toBeInTheDocument();
+  });
+
+  it('deve renderizar a imagem do fã corretamente', () => {
+    const img = screen.getByRole('img', { name: `Foto de ${fanMock.nickname}` });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', fanMock.photoUrl);
+  });
+
+  it('deve renderizar o nível de fã corretamente', () => {
+    const fanLevel = screen.getByText((content, element) => {
+      return content.includes(fanMock.fanLevel);
+    });
+  
+    expect(fanLevel).toBeInTheDocument();
+  });
+  
+  it('deve renderizar o botão de favoritar desabilitado para usuários não logados', () => {
+    const favoriteButton = screen.getByRole('button', { name: /favoritar/i });
+  
+    expect(favoriteButton).toBeInTheDocument();
+    expect(favoriteButton).toBeDisabled();
+    expect(favoriteButton).toHaveAttribute('title', 'Você deve estar logado para favoritar');
+  });
+  
+  
 });
+ 
